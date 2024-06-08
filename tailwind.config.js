03a33ce118e517e1de39e7c718dd9b5051589554
Igwedinma Ifechukwu Divine <64442300+I-Divine@users.noreply.github.com>
2024-06-09 00:54:24 +0100

    Setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-pattern": 'url("./assets/images/SBA_1294 (1).png")',
+      },
+    },
+  },
+  plugins: [],
+};
