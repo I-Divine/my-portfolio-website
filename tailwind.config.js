494ab22e7099cbb751f86d20a83da137ebdada19
Igwedinma Ifechukwu Divine <64442300+I-Divine@users.noreply.github.com>
2024-06-11 21:54:25 +0100

    pretty fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        primary: ['"Ubuntu"', ...defaultTheme.fontFamily.sans],
+        secondary: ['"Mitr"', ...defaultTheme.fontFamily.sans],
+        display: ['"Raleway"', ...defaultTheme.fontFamily.serif],
+      },
       backgroundImage: {
         "hero-pattern": 'url("/assets/images/SBA_1294.png")',
       },
